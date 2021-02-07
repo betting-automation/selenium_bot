6913d47245a0bab00cedf9a38579c05668d1255c
Amit Upreti <a.u.aua937@gmail.com>
2021-02-07 14:14:22 +0545

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
         data = json.loads(f.read())
         proxy = data['PROXY']
         CHROME_DRIVER_PATH = data['CHROMEDRIVER_PATH']
-        get_chromedriver(proxy=proxy, CHROME_DRIVER_PATH=CHROME_DRIVER_PATH)
+        driver = get_chromedriver(proxy=proxy, CHROME_DRIVER_PATH=CHROME_DRIVER_PATH)
+        driver.get('https://api.myip.com')
         input('Please press enter on CMD or terminal to close the browser, or you can also simply close the CMD or Terminal::\n\n\n\n\n\n\n ')
         print('Closing Browser')
